c336e292b5931c039a6af6c2deda9fba7920393f
sqfasd <sqf1225@foxmail.com>
2014-09-26 17:44:53 +0800

    move ibash init work to .bashrc

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ test -L $BASH_IT && rm $BASH_IT
 test -d $BASH_IT && mv $BASH_IT ${BASH_IT}.bak
 ln -s $PROGDIR $BASH_IT
 
-echo "export BASH_IT=$BASH_IT" > $HOME/.bash_profile
-echo "source $BASH_IT/ibash.sh" >> $HOME/.bash_profile
+echo "export BASH_IT=$BASH_IT" >> $HOME/.bashrc
+echo "source $BASH_IT/ibash.sh" >> $HOME/.bashrc
