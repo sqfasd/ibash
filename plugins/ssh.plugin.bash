02130ae2d56a96c0f8840bd77fa70c35014d0b0d
sqfasd <sqf1225@foxmail.com>
2015-08-31 12:41:36 +0800

    simplify ssh plugin

--- plugins/ssh.plugin.bash
+++ plugins/ssh.plugin.bash
@@ -4,21 +4,19 @@ _RSA_PUB_FILE=id_rsa.pub
 _RSA_PUB_PATH=$HOME/.ssh/$_RSA_PUB_FILE
 _SSH_HOST_LIST=$HOME/.ssh/host_list
 
-function iadd_ssh() {
+function isshadd() {
   about 'add entry to ssh config'
   param '1: short name of the host'
   param '2: host name'
   param '3: user'
-  param '4: password'
   group 'ssh'
 
-  if [ $# -ne 4 ]; then
-    echo "usage: $0 <shortname> <host> <user> <password>"
+  if [ $# -ne 3 ]; then
+    echo "usage: $0 <shortname> <host> <user>"
     return
   fi
 
-  iautopass "scp $_RSA_PUB_PATH $3@$2:/tmp" $4
-  iautopass "ssh $3@$2 cat /tmp/$_RSA_PUB_FILE >> ~/.ssh/authorized_keys && rm /tmp/$_RSA_PUB_FILE" $4
+  ssh-copy-id $3@$2
   echo "$1=$3@$2" >> $_SSH_HOST_LIST
 }
 
@@ -32,7 +30,7 @@ function isshlist() {
 function issh() {
   about 'use host short name to ssh'
   group 'ssh'
-  
+
   local hostname=`iget_config $_SSH_HOST_LIST $1`
   ssh $hostname
 }
